c2156c3625ae62add574844d80528239cebf3ebf
akashkumarsoni800 <akashkumarsoni800@gmail.com>
2026-01-03 08:26:10 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- "/" ki jagah "./" karein (Relative path)
+  base: '/', // <--- Yahan se bhi dot (.) hata dein, sirf '/' rakhein
   build: {
     sourcemap: true,
     minify: false,
