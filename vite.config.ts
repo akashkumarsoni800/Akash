83a48a070a5de000da3864e4b9ad7ba2b0af1e91
akashkumarsoni800 <akashkumarsoni800@gmail.com>
2026-01-03 07:21:58 -0800

    Enhance Vite config with build and server options
    Added build and server configurations to Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // <--- YE LINE SABSE ZAROORI HAI
+  base: '/',
+  
+  // --- YE PART ADD KARO ---
+  build: {
+    sourcemap: true, // Isse asli line number dikhega
+    minify: false,   // Isse variable ke asli naam dikhenge (ve, gw nahi dikhega)
+  },
+  server: {
+    host: true, // Network par chalane ke liye
+  }
 })
