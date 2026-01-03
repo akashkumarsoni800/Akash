09276e6e6496f0e4bcf2c37db7bd731dab05a02f
akashkumarsoni800 <akashkumarsoni800@gmail.com>
2026-01-02 19:09:41 -0800

    Update useQueries.ts

--- src/hooks/useQueries.ts
+++ src/hooks/useQueries.ts
@@ -56,6 +56,10 @@ export const useGetCallerUserProfile = () => {
     isFetched: true 
   };
 };
+export const useGetCallerUserRole = () => {
+  return { data: 'admin', isLoading: false }; 
+};
+
 
 
 
